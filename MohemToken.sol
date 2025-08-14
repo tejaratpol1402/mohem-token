fca9f1e32849c14104d209f8d592bbbe7a81cd19
tejaratpol1402 <tejaratpol1402@gmail.com>
2025-08-14 10:38:31 -0700

    Add files via upload
    agriment

---
+++ MohemToken.sol
@@ -0,0 +1,206 @@
+/**
+ *Submitted for verification at BscScan.com on 2025-08-09
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// SafeMath is not needed in Solidity 0.8+ as arithmetic operations are checked by default
+// Included for compatibility with existing code
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        return a - b;
+    }
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) return 0;
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        return a / b;
+    }
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a / b;
+    }
+}
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        return msg.data;
+    }
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract ERC20 is Context, IERC20 {
+    using SafeMath for uint256;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = 18;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from zero address");
+        require(recipient != address(0), "ERC20: transfer to zero address");
+
+        _balances[sender] = _balances[sender].sub(amount, "ERC20: insufficient balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from zero address");
+        require(spender != address(0), "ERC20: approve to zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _setupDecimals(uint8 decimals_) internal {
+        _decimals = decimals_;
+    }
+}
+
+contract TeamToken is ERC20 {
+    event TeamFinanceTokenMint(address owner);
+
+    address private _owner;
+
+    modifier checkIsAddressValid(address ethAddress) {
+        require(ethAddress != address(0), "[Validation] invalid address");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(_msgSender() == _owner, "[Validation] caller is not the owner");
+        _;
+    }
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        uint256 supply,
+        address owner,
+        address feeWallet
+    )
+        checkIsAddressValid(owner)
+        checkIsAddressValid(feeWallet)
+        ERC20(name, symbol)
+    {
+        require(decimals >= 8 && decimals <= 18, "[Validation] Not valid decimals");
+        require(supply > 0, "[Validation] initial supply should be greater than 0");
+
+        _owner = owner;
+        _setupDecimals(decimals);
+        _mint(owner, supply);
+
+        emit TeamFinanceTokenMint(owner);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner checkIsAddressValid(account) {
+        require(amount > 0, "[Validation] mint amount should be greater than 0");
+        _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public {
+        require(amount > 0, "[Validation] burn amount should be greater than 0");
+        require(balanceOf(_msgSender()) >= amount, "[Validation] insufficient balance to burn");
+        _burn(_msgSender(), amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "[Validation] burn from zero address");
+        _transfer(account, address(0), amount);
+    }
+}
